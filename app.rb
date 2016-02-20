b6d879c3c54f29bbf0fbdfe545e4705264adcd46
webg <diyhouse@yandex.ru>
2016-02-20 20:57:37 +0300

    Barber select save

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
@@ -16,6 +17,7 @@ get '/visit' do
 end
 
 post '/visit' do
+
 	@name=params[:username]
 	@phone=params[:phone]
 	@date=params[:datetime]
@@ -41,8 +43,7 @@ post '/visit' do
 	f.write "User: #{@name}, Phone: #{@phone}, Date: #{@date}, Barber: #{@barber}, Color: #{@color} \n"
 	f.close
 	
-	erb "#{@name}, Вы записаны к парикмахеру #{@barber} на #{@date}. В ближайщее время на Ваш номер #{@phone} позвонит наш менджер"
-
+    erb "#{@name}, Вы записаны к парикмахеру #{@barber} на #{@date}. В ближайщее время на Ваш номер #{@phone} позвонит наш менджер"
 end
 
 get '/contacts' do
