21dc156e175d3e7a79d3e6e1e9f66e6eb66755af
webg <diyhouse@yandex.ru>
2016-02-21 15:56:15 +0300

    add barbers db

--- app.rb
+++ app.rb
@@ -5,24 +5,49 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+    db.execute('select * from Barbers where name=?', [name]).length > 0
+end
+
+def seed_db db, barbers
+
+    barbers.each do |barber|
+        if !is_barber_exists? db, barber
+            db.execute 'insert into Barbers (name) values (?)', [barber]
+        end 
+    end
+
+end
+
 def get_db
     db = SQLite3::Database.new 'barbershop.db'
     db.results_as_hash = true
     return db
 end
 
+
 configure do
     db = get_db
-    	db.execute 'CREATE TABLE IF NOT EXISTS
-        	"Users"
-        	(
-            	"id" INTEGER PRIMARY KEY AUTOINCREMENT,
-            	"name" TEXT,
-            	"phone" TEXT,
-            	"datestamp" TEXT,
-            	"barber" TEXT,
-            	"color" TEXT
-        	)'
+    db.execute 'CREATE TABLE IF NOT EXISTS
+       	"Users"
+       	(
+           	"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+           	"name" TEXT,
+           	"phone" TEXT,
+           	"datestamp" TEXT,
+           	"barber" TEXT,
+           	"color" TEXT
+       	)'
+
+    db.execute 'CREATE TABLE IF NOT EXISTS
+        "Barbers"
+            (
+                "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+                "name" TEXT
+            )'
+
+seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehramtraut', 'Ruslan Gandaloev']
+
 end
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
